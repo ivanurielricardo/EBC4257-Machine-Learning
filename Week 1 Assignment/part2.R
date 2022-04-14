3384494b6df1c53d8bc86b2cf2751831e52f0833
Ivan <i.ricardo@student.maastrichtuniversity.nl>
2022-04-14 10:12:25 +0200

    Update part 2

--- Week 1 Assignment/part2.R
+++ Week 1 Assignment/part2.R
@@ -7,10 +7,11 @@ library(ISLR2)
 library(tidyverse)
 library(kernlab)
 
+
 set.seed(3)
 
 
-############# Problem 11 ############# 
+############# Problem 12 ############# 
 
 # We use the Spam dataset from the kernlab library
 data(spam)
@@ -38,12 +39,38 @@ text(tree.spam, pretty = 0, cex = 0.6)
 
 spam.yhat <- predict(tree.spam, spam.test, type = "class")
 plot(spam.yhat, spam.test$type)
-table(spam.yhat, spam.test$type)
+spam.confusion <- table(spam.yhat, spam.test$type)
+spam.confusion
 
-sensitivity <- 752/(752+149)
-specificity <- 1299/(1299+101)
-# Sensitivity is 0.8346 and Specificity is 0.9278
+sensitivity <- spam.confusion[2,2]/sum(spam.confusion[,2])
+specificity <- spam.confusion[1,1]/sum(spam.confusion[,1])
+# Sensitivity is 0.8324 and Specificity is 0.9591
 
-# We can compare this with a pruned tree
+# We can use cross validation to check the pruned trees
+
+cv.spam <- cv.tree(tree.spam)
+plot(cv.spam$size, cv.spam$dev, type = "b")
+
+# Pruning the tree would yield no better results
+
+# Lets assume that pruning the tree would yield better results
 
 
+############# Problem 13 ############# 
+
+# We now find the MNIST dataset
+
+
+devtools::install_github("rstudio/keras") 
+# installing keras 
+# It will first install tensorflow then keras.
+# The above code will install the keras library from the GitHub repository.
+
+install.packages("rstudio/keras")
+
+#loading keras in R 
+library(keras)
+#The R interface to Keras uses TensorFlow as it’s underlying computation engine.
+
+#loading the keras inbuilt mnist dataset
+data<-dataset_mnist()
